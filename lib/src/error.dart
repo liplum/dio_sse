ead1292e9acfdfb51516a43d447091ad43cb3db1
liplum <Li_plum@outlook.com>
2025-09-03 20:57:14 +0800

    Refactor: Simplify EventSource API and improve error handling
    This commit refactors the `EventSource` class to simplify its API and improve error handling.
    
    The key changes are:
    
    - Removed `EventSourceRequestOptions` and `EventSourceRequestOptionsGetter` in favor of a simpler `RequestCallback` typedef.
    - The `EventSource.connect` static method is removed. Users now directly instantiate `EventSource` and call the `start()` method.
    - A new static helper method `EventSource.asCallback` is introduced to easily create a `RequestCallback` from Dio parameters.
    - The `EventSourceException` now includes the original `error` object that caused the exception.
    - Logging no longer includes the URL in the log tag.

--- lib/src/error.dart
+++ lib/src/error.dart
@@ -3,9 +3,10 @@
 /// `EventSource` facilitates the connection, disconnection, and management of SSE streams.
 /// It implements the Singleton pattern to ensure a single instance handles SSE streams throughout the application.
 class EventSourceException implements Exception {
+  final Object? error;
   final String? message;
   final int? statusCode;
   final String? reason;
 
-  const EventSourceException({this.message, this.statusCode, this.reason});
+  const EventSourceException({this.error, this.message, this.statusCode, this.reason});
 }
--- lib/src/sse.dart
+++ lib/src/sse.dart
@@ -57,144 +57,60 @@ class EventSourceOptions {
   });
 }
 
-class EventSourceRequestOptions {
-  final String? method;
-  final Object? data;
-  final Map<String, dynamic>? queryParameters;
-  final CancelToken? cancelToken;
-  final Options? options;
-
-  const EventSourceRequestOptions({this.method, this.data, this.queryParameters, this.cancelToken, this.options});
-}
-
-typedef EventSourceRequestOptionsGetter = Future<EventSourceRequestOptions> Function();
-
 StreamTransformer<Uint8List, List<int>> _unit8Transformer = StreamTransformer.fromHandlers(
   handleData: (data, sink) {
     sink.add(List<int>.from(data));
   },
 );
 
+typedef RequestCallback = Future<Response<ResponseBody>> Function({required CancelToken cancelToken});
+
 class EventSource {
   final _messages = StreamController<EventPack>.broadcast();
   final _errors = StreamController<EventSourceException>.broadcast();
-  final String url;
-  final EventSourceRequestOptionsGetter request;
+  final RequestCallback request;
   final EventSourceOptions options;
+  var _connected = false;
   bool _disposed = false;
   StreamSubscription? _streamSubscription;
   int _maxAttempts = 0;
-  final Dio dio;
-  var _connected = false;
 
   bool get connected => _connected;
 
-  /// Factory method for spawning new instances of `EventSource`.
-  ///
-  /// This method creates and returns a new instance of `EventSource`. It's useful
-  /// for scenarios where multiple, separate `EventSource` instances are needed,
-  /// each operating independently from one another. This allows for different
-  /// SSE connections or functionalities to be managed separately within the same application.
-  ///
-  /// Returns:
-  ///   - A new `EventSource` instance.
-  ///
-  /// Usage Example:
-  /// ```dart
-  /// EventSource EventSourceInstance1 = EventSource.spawn();
-  /// EventSource EventSourceInstance2 = EventSource.spawn();
-  ///
-  ///
-  /// EventSourceInstance1.connect(/* connection parameters */);
-  /// EventSourceInstance2.connect(/* connection parameters */);
-  /// ```
-  ///
-  /// This method is ideal when distinct, isolated instances of `EventSource` are required,
-  /// offering more control over multiple SSE connections.
-  EventSource._({required this.url, required this.dio, required this.request, EventSourceOptions? options})
-    : options = options ?? const EventSourceOptions();
-
-  static Future<EventSourceRequestOptions> _defaultRequestOptions() async {
-    return const EventSourceRequestOptions();
-  }
-
-  /// Establishes a connection to a server-sent event (SSE) stream.
-  ///
-  /// This method sets up a connection to an SSE stream based on the provided URL and connection type.
-  /// It handles the data stream, manages errors, and implements an auto-reconnection mechanism.
-  ///
-  /// Parameters:
-  ///   - `type`: The type of HTTP connection to be used (GET or POST).
-  ///   - `url`: The URL of the SSE stream to connect to.
-  ///   - `header`: HTTP headers for the request. Defaults to accepting 'text/event-stream'.
-  ///   - `onConnectionClose`: Callback function that is called when the connection is closed.
-  ///   - `autoReconnect`: Boolean value that determines if the connection should be
-  ///     automatically reestablished when interrupted. Defaults to `false`.
-  ///   - `reconnectConfig`: Optional configuration for reconnection attempts. Required if `autoReconnect` is enabled.
-  ///   - `onSuccess`: Required callback function that is called upon a successful
-  ///     connection. It provides an `EventSourceResponse` object containing the connection status and data stream.
-  ///   - `onError`: Callback function for handling errors that occur during the connection
-  ///     or data streaming process. It receives an `EventSourceException` object.
-  ///   - `body`: Optional body for POST request types.
-  ///   - `tag`: Optional tag to identify the connection.
-  ///   - `logReceivedData`: Boolean value that determines if received data should be logged. Defaults to `false`.
-  ///   - `files`: Optional list of files to be sent with the request.
-  ///   - `multipartRequest`: Boolean value that determines if the request is a multipart request. Defaults to `false`.
-  ///   - `httpClient`: Optional HTTP client adapter to be used for the connection.
-  ///
-  ///
-  /// The method initializes an HTTP client and a StreamController for managing the SSE data.
-  /// It creates an HTTP request based on the specified `type`, `url`, `header`, and `body`.
-  /// Upon receiving the response, it checks the status code for success (200) and proceeds
-  /// to listen to the stream. It parses each line of the incoming data to construct `EventPack` objects
-  /// which are then added to the stream controller.
-  ///
-  /// The method includes error handling within the stream's `onError` callback, which involves
-  /// invoking the provided `onError` function, adding the error to the stream controller, and
-  /// potentially triggering a reconnection attempt if `autoReconnect` is `true`.
-  ///
-  /// In the case of stream closure (`onDone`), it disconnects the client, triggers the `onConnectionClose`
-  /// callback, and, if `autoReconnect` is enabled, schedules a reconnection attempt after a delay.
-  ///
-  /// Usage Example:
-  /// ```dart
-  /// EventSource EventSource = EventSource.instance;
-  /// EventSource.connect(
-  ///   EventSourceConnectionType.get,
-  ///   'https://example.com/events',
-  ///   onSuccess: (response) {
-  ///     response.stream?.listen((data) {
-  ///       // Handle incoming data
-  ///     });
-  ///   },
-  ///   onError: (exception) {
-  ///     // Handle error
-  ///   },
-  ///   autoReconnect: true
-  ///   reconnectConfig: ReconnectConfig(
-  ///   mode: ReconnectMode.linear || ReconnectMode.exponential,
-  ///   interval: const Duration(seconds: 2),
-  ///   maxAttempts: 5,
-  ///   reconnectCallback: () {},
-  /// ),
-  /// );
-  /// ```
-  ///
-  /// This method is crucial for establishing and maintaining a stable connection to an SSE stream,
-  /// handling data and errors efficiently, and providing a resilient connection experience with
-  /// its auto-reconnect capability.
-  static Future<EventSource> connect(
-    String url, {
-    required Dio dio,
-    EventSourceOptions options = const EventSourceOptions(),
-    EventSourceRequestOptionsGetter request = _defaultRequestOptions,
-  }) async {
-    final eventSource = EventSource._(url: url, dio: dio, options: options, request: request);
-    await eventSource._start(isReconnection: false);
-    return eventSource;
-  }
+  EventSource({required this.request, this.options = const EventSourceOptions()});
 
   CancelToken? _cancelToken;
+
+  static RequestCallback asCallback(
+    Dio dio, {
+    required String url,
+    Options? options,
+    Map<String, dynamic>? queryParameters,
+    Object? data,
+  }) {
+    return ({required CancelToken cancelToken}) async {
+      final former = options ?? Options();
+      return await dio.request<ResponseBody>(
+        url,
+        cancelToken: cancelToken,
+        queryParameters: queryParameters,
+        data: data,
+        options: former.copyWith(
+          method: former.method,
+          headers: {
+            if (former.headers != null) ...former.headers!,
+            "Accept": "text/event-stream",
+            "Cache-Control": "no-cache",
+          },
+          responseType: ResponseType.stream,
+        ),
+      );
+    };
+  }
+
+  Future<void> start() async {
+    await _start(isReconnection: false);
+  }
 
   /// An internal method to handle the connection process.
   /// this is abstracted out to set the `_isExplicitDisconnect` variable to `false` before connecting.
@@ -207,34 +123,17 @@ class EventSource {
       await _streamSubscription?.cancel();
       _streamSubscription = null;
       _connected = false;
-    } else {
-      cancelToken = _cancelToken = CancelToken();
     }
+    cancelToken = _cancelToken = CancelToken();
     _log(_LogCat.info, 'Connection Initiated');
     try {
-      final requestOptions = await request();
-      final formerOptions = requestOptions.options ?? Options();
-      final response = await dio.request<ResponseBody>(
-        url,
-        cancelToken: cancelToken,
-        options: formerOptions.copyWith(
-          method: requestOptions.method,
-          headers: {
-            if (formerOptions.headers != null) ...formerOptions.headers!,
-            "Accept": "text/event-stream",
-            "Cache-Control": "no-cache",
-          },
-          responseType: ResponseType.stream,
-        ),
-      );
+      final response = await request(cancelToken: cancelToken);
       _log(_LogCat.info, 'Connected: ${response.statusCode.toString()}');
 
       _connected = true;
 
       var curPack = _EventPackBuilder();
-      _streamSubscription = response
-          .data
-          ?.stream //
+      _streamSubscription = response.data?.stream
           .transform(_unit8Transformer)
           .transform(const Utf8Decoder())
           .transform(const LineSplitter())
@@ -293,6 +192,7 @@ class EventSource {
               /// Executes the onError function if it is not null
               _addException(
                 EventSourceException(
+                  error: error,
                   message: error.toString(),
                   statusCode: response.statusCode,
                   reason: response.statusMessage,
@@ -306,13 +206,16 @@ class EventSource {
       _connected = false;
       if (error is DioException) {
         final data = error.response?.data;
-        _addException(EventSourceException(
-          statusCode: error.response?.statusCode,
-          reason: error.response?.statusMessage,
-          message: "${data is ResponseBody ? await _streamToString(data.stream) : data}",
-        ));
+        _addException(
+          EventSourceException(
+            error: error,
+            statusCode: error.response?.statusCode,
+            reason: error.response?.statusMessage,
+            message: "${data is ResponseBody ? await _streamToString(data.stream) : data}",
+          ),
+        );
       } else {
-        _addException(EventSourceException(message: error.toString()));
+        _addException(EventSourceException(error: error, message: error.toString()));
       }
       await _stop();
       await _attemptReconnectIfNeeded();
@@ -400,7 +303,7 @@ class EventSource {
 
   /// Logs the given [message] with the corresponding [event] and [tag].
   void _log(_LogCat event, String message) {
-    log('${event.label} $message', name: "EventSource $url");
+    log('${event.label} $message');
   }
 }
 
